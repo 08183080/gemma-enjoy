0282feebda99690013bed4b4d6b8e6c5300a3140
08183080 <houlong.xie@intel.com>
2024-02-22 23:22:09 +0800

    log

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Gemma-enjoy
 ## Experience-log
-- [x] 2/22, 本地下载模型初步跑了2B模型|ollama更方便去跑这些模型
+- [x] 2/22, 本地下载模型初步跑了2B模型 | ollama 更方便去跑这些模型
--- test_gemma.py
+++ test_gemma.py
@@ -4,8 +4,9 @@ tokenizer = AutoTokenizer.from_pretrained("C:\\gemma-2b")
 model = AutoModelForCausalLM.from_pretrained("C:\\gemma-2b")
 
 input_text = "Who are you?"
-input_chi = "你是谁呀?"
-input_ids = tokenizer(input_chi, return_tensors="pt")
+input2 = 'Can you write a poem, please'
+input_chi = "你是谁呀? 回答我"
+input_ids = tokenizer(input2, return_tensors="pt")
 
 outputs = model.generate(**input_ids)
 print(tokenizer.decode(outputs[0]))
