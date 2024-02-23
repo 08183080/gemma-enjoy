cd8ed2396d91e07ab27757e9ec8a8afba6149090
08183080 <houlong.xie@intel.com>
2024-02-23 08:43:57 +0800

    [test] max_token

--- test_gemma.py
+++ test_gemma.py
@@ -3,12 +3,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("C:\\gemma-2b")
 model = AutoModelForCausalLM.from_pretrained("C:\\gemma-2b")
 
-input_text = "Who are you?"
-input2 = 'Can you write a poem, please'
-input_chi = "你是谁呀? 回答我"
-input_ids = tokenizer(input2, return_tensors="pt")
+inpu_1= "Who are you?"
+input_2 = 'please write a poem about love'
+input_3 = "你是谁呀? 回答我"
 
-outputs = model.generate(**input_ids)
+input_ids = tokenizer(input_2, return_tensors="pt")
+
+outputs = model.generate(**input_ids, max_new_tokens=200)
 print(tokenizer.decode(outputs[0]))
-
-print(outputs)
