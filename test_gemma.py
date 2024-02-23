9e51ebf67e0af0e53ef86f8475e10bd43c6f8f30
08183080 <houlong.xie@intel.com>
2024-02-23 09:11:09 +0800

    [prompt] add prompt

--- test_gemma.py
+++ test_gemma.py
@@ -3,11 +3,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("C:\\gemma-2b")
 model = AutoModelForCausalLM.from_pretrained("C:\\gemma-2b")
 
-inpu_1= "Who are you?"
+input_1= "Who are you?"
 input_2 = 'please write a poem about love'
 input_3 = "你是谁呀? 回答我"
+input_4 = 'how to become a life winner?'
+input_5 = 'how ca i become a successful python coder, plese give me some tips'
 
-input_ids = tokenizer(input_2, return_tensors="pt")
+input_ids = tokenizer(input_5, return_tensors="pt")
 
-outputs = model.generate(**input_ids, max_new_tokens=200)
+outputs = model.generate(**input_ids, max_new_tokens=300)
+
 print(tokenizer.decode(outputs[0]))
